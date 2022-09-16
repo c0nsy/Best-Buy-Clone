1f195efd0fb2e42c3f9281db8edf106dc3f13134
Connor Logan <connorlogan00@gmail.com>
2022-09-15 21:42:42 -0400

    working on product preview in ProductList..

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,4 +1,5 @@
 import {Link} from "react-router-dom"
+import Laptop1 from "./assets/LaptopImages/LenovoLaptop1.png"
 export default function ProductList({products,title}){
     return(
         <div className="product-list">
@@ -6,9 +7,10 @@ export default function ProductList({products,title}){
             {/* could maybe use filter by tag to filter out each product(?) */}
             {/* filters like, laptop, mice, desktop, laptop cases, etc */}
             {products.map((product) =>
-                <Link to = {`/products/${product.id}`}>
+                <Link to = {`/products/${product.id}`} className="preview-link">
                     <div className="product-preview" key={product.id}>
                         <h1>{product.title}</h1>
+                        <img src={product.image} alt="" />
                     </div>
                 </Link>
                 
