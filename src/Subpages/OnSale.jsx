f965532a36e4aebd004c9931ed0572c12cf4a2e0
Connor Logan <connorlogan00@gmail.com>
2022-09-26 15:29:31 -0400

    onsale skeleton

--- src/Subpages/OnSale.jsx
+++ src/Subpages/OnSale.jsx
@@ -1,8 +1,47 @@
-
+import OnSaleBanner from "../assets/onsalebanner.png"
 export default function OnSale(){
     return(
         <div className="on-sale">
-            <h1>On Sale</h1>
+            <img src={OnSaleBanner} className="deals-banner"/>
+            <h3 className="deals-text">On Sale</h3>
+            <div className="popular-deals--wrapper">
+                <div>
+                    <img src="" alt="" />
+                    <h4>On Sale Laptops</h4>
+                    <p>summery here</p>
+                    <p>link to popular-laptop-deals TBM</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h4>On Sale Monitors</h4>
+                    <p>summary here</p>
+                    <p>link to popular-monitor-deals</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h4>On Sale Gaming Gear</h4>
+                    <p>summary here</p>
+                    <p>link to popular-gaminggear-deals</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h4>On Sale Stero Systems</h4>
+                    <p>summary here</p>
+                    <p>link to popular-sterio-deals</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h4>On Sale TV's</h4>
+                    <p>summary here</p>
+                    <p>link to popular-tvs-deals</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h4>On Sale Appliances</h4>
+                    <p>summary here</p>
+                    <p>link to popular-appliances-deals</p>
+                </div>
+            </div>
         </div>
     )
 }
