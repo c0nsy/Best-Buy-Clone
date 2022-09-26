48b6e2e2a8429ca32fc56ef293b4ce447e881dee
Connor Logan <connorlogan00@gmail.com>
2022-09-26 16:21:47 -0400

    added comments for todo

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,6 +3,9 @@ import useFetch from "./useFetch"
 export default function Home(){
     const {data: product, isPending, error} = useFetch("http://localhost:8000/products")
     return(
+        // reformat this page so its more inline with bestbuy.ca site
+        // add component 'pictures' that are like top deals or whatever, the square ones
+        // under ^^ have a product showcase, laptops, tvs, monitors etc
         <div className="home">
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
