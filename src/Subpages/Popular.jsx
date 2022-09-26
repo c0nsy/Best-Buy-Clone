6528f52726730cb4488c37517a8b6db3eb36a7f6
Connor Logan <connorlogan00@gmail.com>
2022-09-26 15:24:59 -0400

    created a skeleton for popular deals

--- src/Subpages/Popular.jsx
+++ src/Subpages/Popular.jsx
@@ -1,8 +1,47 @@
-
+import PopularBanner from "../assets/populardeals.png"
 export default function Popular(){
     return(
-        <div className="popular">
-            <h1>Popular</h1>
+        <div className="popular-deals">
+            <img src={PopularBanner} className="popular-banner"/>
+            <h3 className="popular-title">Popular Deals</h3>
+            <div className="popular-deals--wrapper">
+                <div>
+                    <img src="" alt="" />
+                    <h4>Popular Laptops</h4>
+                    <p>summery here</p>
+                    <p>link to popular-laptop-deals TBM</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h4>Popular Monitors</h4>
+                    <p>summary here</p>
+                    <p>link to popular-monitor-deals</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h4>Popular Gaming Gear</h4>
+                    <p>summary here</p>
+                    <p>link to popular-gaminggear-deals</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h4>Popular Stero Systems</h4>
+                    <p>summary here</p>
+                    <p>link to popular-sterio-deals</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h4>Popular TV's</h4>
+                    <p>summary here</p>
+                    <p>link to popular-tvs-deals</p>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <h4>Popular Appliances</h4>
+                    <p>summary here</p>
+                    <p>link to popular-appliances-deals</p>
+                </div>
+            </div>
         </div>
     )
 }
