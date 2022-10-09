187379c1294da2a9d4bb21e7526e75aac9fec973
Connor Logan <65863220+c0nsy@users.noreply.github.com>
2022-10-09 10:23:45 -0400

    added stuff to home page
    revamping home page so its like actual best buy

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,6 @@
 import ProductList from "./ProductList"
+import TopDeals from "./assets/Home/topdealshome.png"
+import Gaming from "./assets/Home/bestbuygaming.png"
 import useFetch from "./useFetch"
 export default function Home(){
     const {data: product, isPending, error} = useFetch("http://localhost:8000/products")
@@ -7,9 +9,13 @@ export default function Home(){
         // add component 'pictures' that are like top deals or whatever, the square ones
         // under ^^ have a product showcase, laptops, tvs, monitors etc
         <div className="home">
-            {error && <div>{error}</div>}
+            {/* {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
-            {product && <ProductList products={product} title="Inventory" />}
+            {product && <ProductList products={product} title="Inventory" />} */}
+            {/* have four different sized images, deals, about, gaming, and laptop that link to corresponding pages */}
+            {/* gonna have to set a max width */}
+            <img src={TopDeals} alt="" />
+            <img src={Gaming} alt="" />
         </div>
     )
 }
